4245bc7360f016cfab84fef34550962db939f279
jos-h20 <josh_overly@yahoo.com>
2016-11-20 21:37:16 -0800

    add labels to form

--- filmlist/films/forms.py
+++ filmlist/films/forms.py
@@ -5,3 +5,8 @@ class FilmForm(forms.ModelForm):
     class Meta:
         model = Film
         fields = ['title', 'year_prod', 'genre', 'img_url']
+        labels = {'title': 'Film Title',
+                  'year_prod': 'Year',
+                  'genre': 'Genre',
+                  'img_url': 'Image',
+                  }
